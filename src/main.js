3e38621f51fb53cd12f1666558fec963bc74dec1
TearsWaiting <1773432827@qq.com>
2020-04-19 22:27:25 +0800

    test modified3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router';
-// test
-Vue.use(VueRouter);
+import './plugins/element.js'
 
 
 Vue.config.productionTip = false
